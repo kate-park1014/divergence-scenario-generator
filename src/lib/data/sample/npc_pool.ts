11a46cdc99908056f214138b528f66465e1ce118
kate-park1014 <kate@atomrigs.io>
2026-04-16 22:36:38 +0900

    chore: update npc info

--- src/lib/data/sample/npc_pool.ts
+++ src/lib/data/sample/npc_pool.ts
@@ -154,3 +154,131 @@ export const npc_pool = [
 		role: { type: 'Dealer', weapon: '' }
 	}
 ];
+
+export const npc_2_pools = [
+	{
+		id: 'actor_06',
+		key: 'Eirin',
+		name: {
+			korean: '에이린',
+			english: 'Eirin',
+			japanese: 'エイリン',
+			chinese: '艾琳',
+			french: 'Eirin',
+			spanish: 'Eirin',
+			vietnamese: 'Eirin',
+			thai: 'เอ린',
+			hindi: 'इरिन'
+		},
+		description: '',
+		face_image_url:
+			'https://imagedelivery.net/WTEqQNGFb-l-IHS6IGnzUQ/d8c86048-41b1-4ae6-4d93-91fe01258600/public',
+		battle_sprite_url: '',
+		role: { type: 'Dealer', weapon: '' }
+	},
+	{
+		id: 'actor_07',
+		key: 'Eiric',
+		name: {
+			korean: '에이릭',
+			english: 'Eiric',
+			japanese: 'エイリック',
+			chinese: '埃里克',
+			french: 'Eiric',
+			spanish: 'Eiric',
+			vietnamese: 'Eiric',
+			thai: 'เอริค',
+			hindi: 'एरिक'
+		},
+		description: '',
+		face_image_url:
+			'https://imagedelivery.net/WTEqQNGFb-l-IHS6IGnzUQ/5d0a3fe4-8501-4bff-cadd-902ca5aa0300/public',
+		battle_sprite_url: '',
+		role: { type: 'Dealer', weapon: '' }
+	},
+	{
+		id: 'actor_08',
+		key: 'Roundy',
+		monster_id: 'mon_ef37da02-a6ef-4074-a8a0-bf55b4cc5b64',
+		name: {
+			korean: '라운디',
+			english: 'Roundy',
+			japanese: 'ラウンディ',
+			chinese: '朗迪',
+			french: 'Roundy',
+			spanish: 'Roundy',
+			vietnamese: 'Roundy',
+			thai: 'รา운ดี้',
+			hindi: 'राउंडी'
+		},
+		description: '',
+		face_image_url:
+			'https://imagedelivery.net/WTEqQNGFb-l-IHS6IGnzUQ/9c99a7b0-5bc4-45d0-11bb-15dd86ae0700/public',
+		battle_sprite_url: '',
+		attack_sprite_url: '',
+		role: { type: 'Dealer', weapon: '' }
+	},
+	{
+		id: 'actor_09',
+		key: 'Ice',
+		monster_id: 'mon_d975c5f1-33f7-4334-aa36-8af56bbb61d3',
+		name: {
+			korean: '얼음',
+			english: 'Ice',
+			japanese: '氷',
+			chinese: '冰',
+			french: 'Glace',
+			spanish: 'Hielo',
+			vietnamese: 'Băng',
+			thai: 'น้ำแข็ง',
+			hindi: 'बर्फ़'
+		},
+		description: '',
+		face_image_url:
+			'https://imagedelivery.net/WTEqQNGFb-l-IHS6IGnzUQ/c9fb8bdd-bb4b-4523-1746-3bb17c64f400/public',
+		battle_sprite_url: '',
+		attack_sprite_url: '',
+		role: { type: 'Dealer', weapon: '' }
+	},
+	{
+		id: 'actor_10',
+		key: 'Astri',
+		name: {
+			korean: '아스트리',
+			english: 'Astri',
+			japanese: 'アストリ',
+			chinese: '阿斯特里',
+			french: 'Astri',
+			spanish: 'Astri',
+			vietnamese: 'Astri',
+			thai: 'อ스트리',
+			hindi: 'अस्त्री'
+		},
+		description: '',
+		face_image_url:
+			'https://imagedelivery.net/WTEqQNGFb-l-IHS6IGnzUQ/0496af1b-19c2-43b2-c579-38a283320a00/public',
+		battle_sprite_url: '',
+		attack_sprite_url: '',
+		role: { type: 'Dealer', weapon: '' }
+	},
+	{
+		id: 'actor_11',
+		key: 'Asrin',
+		name: {
+			korean: '아스린',
+			english: 'Asrin',
+			japanese: 'アスリン',
+			chinese: '阿斯琳',
+			french: 'Asrin',
+			spanish: 'Asrin',
+			vietnamese: 'Asrin',
+			thai: 'อัสริน',
+			hindi: 'अस्रिन'
+		},
+		description: '',
+		face_image_url:
+			'https://imagedelivery.net/WTEqQNGFb-l-IHS6IGnzUQ/53eff152-4c8e-4668-375f-0cb0b0d02300/public',
+		battle_sprite_url: '',
+		role: { type: 'Dealer', weapon: '' }
+	}
+];
